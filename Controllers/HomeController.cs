8c72bf3189bccead0d6acb9e852f6e994ff98f23
mohammadkaif11 <mohammadkaif0211@gmail.com>
2022-01-24 19:38:31 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using notesappp.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
+namespace notesappp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NoteDataContext noteDataContext=new NoteDataContext();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public JsonResult Getdata()
+        {
+            List<Note> notes = noteDataContext.Allnotes();
+            return Json(notes);
+        }
+
+
+        
+        [HttpPost]
+        public bool DeleteData(int id)
+        {
+            int k = noteDataContext.ddata(id);
+            if (k > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        [HttpPost]
+        public bool PostData(string title,string description)
+        { 
+            Note note = new Note();
+            note.Title = title;
+            note.Description=description;
+            int k = noteDataContext.pdata(note);
+            if (k > 0)
+            {
+                return true;            
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+       
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/NoteDataContext.cs
@@ -0,0 +1,75 @@
+﻿using System.Data.SqlClient;
+namespace notesappp.Models
+{
+    public class NoteDataContext
+    {
+        public List<Note> Allnotes()
+        {
+            List<Note> notes = new List<Note>();
+            string cs = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Testing;Integrated Security=true;";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "select * from Kaif";
+            SqlCommand sqlCommand = new SqlCommand(query, con);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+               Note note = new Note();
+                note.Id = sqlDataReader.GetInt32(0);
+                note.Title=sqlDataReader.GetString(1);
+                note.Description=sqlDataReader.GetString(2);
+                notes.Add(note);
+            }
+            con.Close();
+            return notes;
+        }
+
+
+        
+        public int pdata(Note note)
+        {
+            string cs = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Testing;Integrated Security=true;";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+            string query = "insert into Kaif values(@title,@description)";
+            SqlCommand sqlCommand = new SqlCommand(query, con);
+            sqlCommand.Parameters.AddWithValue("@title", note.Title);
+            sqlCommand.Parameters.AddWithValue("@description",note.Description);
+            int k = sqlCommand.ExecuteNonQuery();
+            con.Close();
+
+            if (k > 0)
+            {
+                return k;
+            }
+            else
+            {
+                return 0; 
+            }
+        }
+
+
+        public int ddata(int id)
+        {
+                                            string cs = "Data Source=(localdb)\\ProjectModels;Initial Catalog=Testing;Integrated Security=true;";
+                                            SqlConnection con = new SqlConnection(cs);
+                                            con.Open();
+                                            string query = "delete from Kaif where id=@id";
+                                            SqlCommand sqlCommand = new SqlCommand(query, con);
+                                            sqlCommand.Parameters.AddWithValue("@id",id);
+                                            int k = sqlCommand.ExecuteNonQuery();
+                                            con.Close();
+                                            if (k > 0)
+                                            {
+                                                return k;
+                                            }
+                                            else
+                                            {
+                                                return 0;
+                                            }
+
+        }
+        
+    } 
+}
